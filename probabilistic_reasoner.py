94f9b08b95ad75a13632bf86bd147ae529669b0e
verheesj <jonathan@verhe.es>
2023-07-03 17:41:56 +0000

    *  a simple inference rule: if we know "A implies B" and "A is true", we can infer that "B is true" * We can then add facts to the knowledge base and use the infer_rule method to infer new facts

--- probabilistic_reasoner.py
+++ probabilistic_reasoner.py
@@ -27,6 +27,34 @@ class ProbabilisticReasoner:
             truth_value = query in self.kb.facts
             return truth_value, prob
         return None, None
+        
+    def infer_rule(self, a, b):
+        """ Infer a fact from the knowledge base using a rule """
+
+        # If we know "A => B" and "A is True", then we can infer "B is True"
+
+        a_prob = self.kb.ask(a)
+
+        if a_prob != "Fact not found" and a_prob > 0.5:  # Let's say A is True if its probability > 0.5
+            self.kb.tell(b, 1)  # Infer B is True and add to the knowledge base
+            return True
+    
+        return False
+
+
+    def resolve_ambiguity(self, fact):
+        """ Resolve ambiguity in a fact """
+        # If fact exists in knowledge base, no ambiguity
+        if self.kb.ask(fact) != "Fact not found":
+            return fact
+        
+        # If fact doesn't exist, check its opposite
+        opposite_fact = 'not ' + fact if 'not ' not in fact else fact.replace('not ', '')
+        if self.kb.ask(opposite_fact) != "Fact not found":
+            return opposite_fact
+
+        return "Fact not found"
+
 
 
 
